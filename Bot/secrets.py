762400a3395bc0e6a221c05c2838658191c07da7
Larko_0 <larionov1001@gmail.com>
2021-03-21 03:00:44 +0200

    One action

--- Bot/secrets.py
+++ Bot/secrets.py
@@ -1,1 +1,1 @@
-TOKEN = '1668560807:AAGop7mPGMxMVhwyTGMkygxYLOvelL4bNvw'  # вставь токен своего бота, созданного в BotFather, сюда
+TOKEN = ''  # вставь токен своего бота, созданного в BotFather, сюда
